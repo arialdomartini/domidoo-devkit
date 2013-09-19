a5dc66b215a1b45f5a9f420cc13c063a22057b0f
Arialdo Martini <arialdomartini@gmail.com>
2013-09-20 00:27:02 +0200

    build.sh installs python, git and ansible, then configure the remote server for a simple ansible access

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 domidoo-devkit
 ==============
 
-python developers kit for domidoo project
+This is the python developers kit for the Domidoo project.
+This package contains what's needed to develop Domidoo and deploy it on a dedicate server.
+
+
+Requirements
+============
+A Mac
+[homebrew](http://brew.sh/) installed
+The ssh password for (a) dedicate domidoo server's root user.
+
+
+Build
+=====
+Clone this repo. Then run
+
+    ./build.sh
+
+You will be asked for the remote server root's password.
+After the first run, you should be able to ssh and ansible the remote server with no password, by the means of your ssh key.
+ 
---
+++ build.sh
@@ -0,0 +1,33 @@
+set -e
+set -u
+
+echo -e "> Installing git..."
+brew install git
+
+echo -e "\n> Installing python..."
+brew install python
+
+
+echo -e "\n> Installing ansible..."
+pip install paramiko
+pip install pyyaml
+pip install jinja2
+brew install https://raw.github.com/mnot/homebrew-stuff/master/ansible.rb
+
+
+echo -e "\n> Configuring ansible..."
+mkdir -p /usr/local/etc/ansible
+cp hosts /usr/local/etc/ansible/hosts
+chmod 644 /usr/local/etc/ansible/hosts
+
+echo -e "\n> Setting permissions on remote server"
+cat ~/.ssh/id_rsa.pub | ssh root@37.247.55.31 "mkdir -p ~/.ssh; cat >> ~/.ssh/authorized_keys"
+
+echo -e "\n> Testing ansible..."
+ansible all -m ping -u root
+
+echo -e "\n\nAll done."
+echo "git, python and ansible are installed."
+
+
+

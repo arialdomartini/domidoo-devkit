5e0e900aca5e8b739b1a2a9f21ac908f6b613756
Arialdo Martini <arialdomartini@gmail.com>
2013-09-20 22:52:22 +0200

    ansible init playbook creates a remote sudoer user

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 set -e
 set -u
 
+SERVER=37.247.55.31
+
 echo -e "> Installing git..."
 brew install git
 
@@ -21,13 +23,20 @@ cp hosts /usr/local/etc/ansible/hosts
 chmod 644 /usr/local/etc/ansible/hosts
 
 echo -e "\n> Setting permissions on remote server"
-cat ~/.ssh/id_rsa.pub | ssh root@37.247.55.31 "mkdir -p ~/.ssh; cat > ~/.ssh/authorized_keys"
+cat ~/.ssh/id_rsa.pub | ssh root@${SERVER} "mkdir -p ~/.ssh; cat > ~/.ssh/authorized_keys"
 
 echo -e "\n> Testing ansible..."
 ansible all -m ping -u root
 
-echo -e "\n\nAll done."
-echo "git, python and ansible are installed."
+echo -e "\n> Creating the user ${USER} in the remote server"
+ansible-playbook init.yml -u root
+
+
+echo -e "\n\nAmazing!"
+echo -e "git, python and ansible are installed on this machine."
+echo -e "The user ${USER} can ssh the remote $SERVER and control it with ansible"
+echo -e "\n${USER} is a remote machine sudoer, and can even login as root."
+echo -e "Too much power and no control may be harmful. Ansible will be your best friend." 
 
 
 

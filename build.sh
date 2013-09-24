83c4386240e658a764e81d8edca3ec46f561b08b
Arialdo Martini <arialdomartini@gmail.com>
2013-09-24 23:13:18 +0200

    Using playbooks and a better colored log

--- build.sh
+++ build.sh
@@ -11,24 +11,28 @@ reset="\033[0m"
 
 function msg() {
     message=$1
-    echo -e "${green}> ${message}...${reset}"
+    echo -e "${green}${message}${reset}"
+}
+
+function log() {
+    msg "> ${1}"
 }
 
 function brew_install() {
     package=$1
     if [ $# -eq 2 ]; then name=${2}; else name=${package}; fi
-    has_package=`which ${package}`
+    has_package=`which ${name}`
     if [ -z ${has_package} ]; then
-	msg "Brewing ${name}"
+	log "Brewing ${name}"
 	brew install ${package}
     else
-	msg "${name} already installed"
+	log ">${name} already installed"
     fi
 }
 
 has_brew=`which brew`
 if [ -z ${has_brew} ]; then
-    msg "Installing brew"
+    log "Installing brew"
     ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 fi
 
@@ -36,44 +40,34 @@ brew_install "git"
 brew_install "python"
 brew_install "virtualenv"
 
-msg "Installing and configuring ansible"
+log "Installing and configuring ansible"
 pip install paramiko
 pip install pyyaml
 pip install jinja2
 
 brew_install "https://raw.github.com/arialdomartini/homebrew-stuff/master/ansible.rb" "ansible"
 
-msg "Configuring ansible"
+log "Configuring ansible"
 mkdir -p /usr/local/etc/ansible
 cp hosts /usr/local/etc/ansible/hosts
 chmod 644 /usr/local/etc/ansible/hosts
 
-msg "Setting permissions on remote server"
+log "Setting permissions on remote server authorizing ${USER}'s ssh key"
 cat ~/.ssh/id_rsa.pub | ssh root@${SERVER} "mkdir -p ~/.ssh; cat > ~/.ssh/authorized_keys"
 
-#echo -e "\n> Testing ansible..."
-#ansible all -m ping -u root
+ansible-playbook init.yml -u root
 
-#echo -e "\n> Installing base packages in the remote server"
-#ansible -v prod -a "apt-get update" -u root
-#ansible -v prod -a "apt-get -y install python-apt" -u root
-#ansible -v prod -a "apt-get -y install python-pip python-dev build-essential" -u root
-#ansible -v prod -a "pip install --upgrade pip" -u root
-#ansible -v prod -a "pip install --upgrade virtualenv" -u root
+msg "Accelerated mode can be used"
 
-#echo -e "\n> Installing packages for accelerating ansible"
-#ansible -v prod -a "pip install --pre python-keyczar" -u root
-#pip install --pre python-keyczar
-
-#echo -e "\n> Installing services on remote server"
-#ansible-playbook init.yml -u root
+log "Creating a remote user ${USER} and configuring it as an administrator"
+ansible-playbook account.yml -u root
 
 
-#echo -e "\n\Sweet!"
-#echo -e "git, python and ansible are installed on this machine."
-#echo -e "The user ${USER} can ssh the remote $SERVER and control it with ansible"
-#echo -e "\n${USER} is a remote machine sudoer, and can even login as root."
-#echo -e "Too much power and no control may be harmful. Ansible will be your best friend." 
+msg "Sweet!"
+msg "brew, git, python and ansible are installed on this machine."
+msg "The user ${USER} can ssh the remote $SERVER and control it with ansible"
+msg "\n${USER} is a remote machine sudoer, and can even login as root."
+msg "Too much power and no control may be harmful. Ansible will be your best friend." 
 
 
 
